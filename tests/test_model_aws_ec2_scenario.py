9b3682dec990132403b303b9e9c88818968b447e
Nico Coetzee <nicc777@gmail.com>
2021-12-22 05:20:52 +0100

    test: #6 - Added tests for various ExtractLogic implementations on close to real-world data

--- tests/test_model_aws_ec2_scenario.py
+++ tests/test_model_aws_ec2_scenario.py
@@ -1,7 +1,8 @@
 import sys
 import os
-import datetime
+from datetime import datetime
 from dateutil.tz import tzutc
+import copy
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 print('sys.path={}'.format(sys.path))
 
@@ -18,14 +19,14 @@ class MockBoto3Ec2Client:
             'Reservations': [
                 {
                     'Groups': [], 
-                    'Instances': [
+                    'Instances': [  # Reservations[].Instances[]
                         {
                             'AmiLaunchIndex': 0, 
                             'ImageId': 'ami-0a8c5ad81884f3891', 
                             'InstanceId': 'i-00000000000000000', 
                             'InstanceType': 't3.micro', 
                             'KeyName': 'dummy-key', 
-                            'LaunchTime': datetime.datetime(2021, 11, 28, 11, 32, 25, tzinfo=tzutc()), 
+                            'LaunchTime': datetime(2021, 11, 28, 11, 32, 25, tzinfo=tzutc()), 
                             'Monitoring': {
                                 'State': 'disabled'
                             }, 
@@ -38,8 +39,9 @@ class MockBoto3Ec2Client:
                             'PrivateIpAddress': '192.168.5.209', 
                             'ProductCodes': [], 
                             'PublicDnsName': '', 
-                            'State': {
-                                'Code': 80, 'Name': 'stopped'
+                            'State': {  # Reservations[].Instances[].State
+                                'Code': 80,         # Reservations[].Instances[].State.Code
+                                'Name': 'stopped'   # Reservations[].Instances[].State.Name
                             }, 
                             'StateTransitionReason': 'User initiated (2021-12-01 05:41:59 GMT)', 
                             'SubnetId': 'subnet-00000000000000000', 
@@ -49,7 +51,7 @@ class MockBoto3Ec2Client:
                                 {
                                     'DeviceName': '/dev/xvda', 
                                     'Ebs': {
-                                        'AttachTime': datetime.datetime(2021, 11, 28, 11, 32, 26, tzinfo=tzutc()), 
+                                        'AttachTime': datetime(2021, 11, 28, 11, 32, 26, tzinfo=tzutc()), 
                                         'DeleteOnTermination': True, 
                                         'Status': 'attached', 
                                         'VolumeId': 'vol-00000000000000000'
@@ -67,7 +69,7 @@ class MockBoto3Ec2Client:
                             'NetworkInterfaces': [
                                 {
                                     'Attachment': {
-                                        'AttachTime': datetime.datetime(2021, 11, 28, 11, 32, 25, tzinfo=tzutc()), 
+                                        'AttachTime': datetime(2021, 11, 28, 11, 32, 25, tzinfo=tzutc()), 
                                         'AttachmentId': 'eni-attach-00000000000000000', 
                                         'DeleteOnTermination': True, 
                                         'DeviceIndex': 0, 
@@ -143,7 +145,7 @@ class MockBoto3Ec2Client:
                             }, 
                             'PlatformDetails': 'Linux/UNIX', 
                             'UsageOperation': 'RunInstances', 
-                            'UsageOperationUpdateTime': datetime.datetime(2021, 11, 28, 11, 32, 25, tzinfo=tzutc()), 
+                            'UsageOperationUpdateTime': datetime(2021, 11, 28, 11, 32, 25, tzinfo=tzutc()), 
                             'PrivateDnsNameOptions': {
                                 'HostnameType': 'ip-name', 
                                 'EnableResourceNameDnsARecord': True, 
@@ -164,7 +166,7 @@ class MockBoto3Ec2Client:
                             'InstanceType': 
                             't2.micro', 
                             'KeyName': 'dummy-key', 
-                            'LaunchTime': datetime.datetime(2021, 11, 28, 11, 18, 46, tzinfo=tzutc()), 
+                            'LaunchTime': datetime(2021, 11, 28, 11, 18, 46, tzinfo=tzutc()), 
                             'Monitoring': {
                                 'State': 'disabled'
                             }, 
@@ -189,7 +191,7 @@ class MockBoto3Ec2Client:
                                 {
                                     'DeviceName': '/dev/xvda', 
                                     'Ebs': {
-                                        'AttachTime': datetime.datetime(2021, 11, 28, 11, 2, 12, tzinfo=tzutc()), 
+                                        'AttachTime': datetime(2021, 11, 28, 11, 2, 12, tzinfo=tzutc()), 
                                         'DeleteOnTermination': True, 
                                         'Status': 'attached', 
                                         'VolumeId': 'vol-11111111111111111'
@@ -207,7 +209,7 @@ class MockBoto3Ec2Client:
                             'NetworkInterfaces': [
                                 {
                                     'Attachment': {
-                                        'AttachTime': datetime.datetime(2021, 11, 28, 11, 2, 11, tzinfo=tzutc()), 
+                                        'AttachTime': datetime(2021, 11, 28, 11, 2, 11, tzinfo=tzutc()), 
                                         'AttachmentId': 'eni-attach-11111111111111111', 
                                         'DeleteOnTermination': True, 
                                         'DeviceIndex': 0, 
@@ -283,7 +285,7 @@ class MockBoto3Ec2Client:
                             }, 
                             'PlatformDetails': 'Linux/UNIX', 
                             'UsageOperation': 'RunInstances', 
-                            'UsageOperationUpdateTime': datetime.datetime(2021, 11, 28, 11, 2, 11, tzinfo=tzutc()), 
+                            'UsageOperationUpdateTime': datetime(2021, 11, 28, 11, 2, 11, tzinfo=tzutc()), 
                             'PrivateDnsNameOptions': {
                                 'HostnameType': 'ip-name', 
                                 'EnableResourceNameDnsARecord': True, 
@@ -303,7 +305,7 @@ class MockBoto3Ec2Client:
                             'InstanceId': 'i-22222222222222222', 
                             'InstanceType': 't4g.micro', 
                             'KeyName': 'dummy-key', 
-                            'LaunchTime': datetime.datetime(2021, 11, 28, 10, 47, 27, tzinfo=tzutc()), 
+                            'LaunchTime': datetime(2021, 11, 28, 10, 47, 27, tzinfo=tzutc()), 
                             'Monitoring': {
                                 'State': 'disabled'
                             }, 
@@ -328,7 +330,7 @@ class MockBoto3Ec2Client:
                                 {
                                     'DeviceName': '/dev/xvda', 
                                     'Ebs': {
-                                        'AttachTime': datetime.datetime(2021, 11, 28, 10, 47, 28, tzinfo=tzutc()), 
+                                        'AttachTime': datetime(2021, 11, 28, 10, 47, 28, tzinfo=tzutc()), 
                                         'DeleteOnTermination': True, 
                                         'Status': 'attached', 
                                         'VolumeId': 'vol-22222222222222222'
@@ -342,7 +344,7 @@ class MockBoto3Ec2Client:
                             'NetworkInterfaces': [
                                 {
                                     'Attachment': {
-                                        'AttachTime': datetime.datetime(2021, 11, 28, 10, 47, 27, tzinfo=tzutc()), 
+                                        'AttachTime': datetime(2021, 11, 28, 10, 47, 27, tzinfo=tzutc()), 
                                         'AttachmentId': 'eni-attach-22222222222222222', 
                                         'DeleteOnTermination': True, 
                                         'DeviceIndex': 0, 
@@ -419,7 +421,7 @@ class MockBoto3Ec2Client:
                             'BootMode': 'uefi', 
                             'PlatformDetails': 'Linux/UNIX', 
                             'UsageOperation': 'RunInstances', 
-                            'UsageOperationUpdateTime': datetime.datetime(2021, 11, 28, 10, 47, 27, tzinfo=tzutc()), 
+                            'UsageOperationUpdateTime': datetime(2021, 11, 28, 10, 47, 27, tzinfo=tzutc()), 
                             'PrivateDnsNameOptions': {
                                 'HostnameType': 'ip-name', 
                                 'EnableResourceNameDnsARecord': True, 
@@ -449,8 +451,99 @@ class MockBoto3Ec2Client:
         }
 
 
+class InstanceStateExtractLogic(ExtractLogic):
+
+    def extract(self, raw_data)->dict:
+        """
+            Expecting a dict with the Instance data - # Reservations[].Instances[].raw_data with the following in raw_data:
+
+            'State': {
+                'Code': 80,
+                'Name': 'stopped'
+            }, 
+        """
+        state = dict()
+        state['Code'] = None
+        state['Name'] = 'unknown'
+        if 'State' in raw_data:
+            state = raw_data['State']
+        return state
+
+
+class InstanceNameExtractLogic(ExtractLogic):
+
+    def extract(self, raw_data)->dict:
+        """
+            Expecting a dict with the Instance data - # Reservations[].Instances[].raw_data with the following in raw_data:
+
+            'Tags': [
+                {
+                    'Key': 'Name', 
+                    'Value': 'machine-01'
+                }
+            ], 
+        """
+        name_data = dict()
+        name_value = '-'
+        for tag_data in raw_data['Tags']:
+            if tag_data['Key'] == 'Name':
+                name_value = tag_data['Value']
+        name_data['Name'] = name_value
+        return name_data
+
+
+class InstanceExtractLogic(ExtractLogic):
+
+    def extract(self, raw_data)->dict:
+        # Expecting the raw response from the AWS EC2 API call "describe-instances"  - Reservations[].Instances[].raw_data
+        instances = dict()
+        for reservation in raw_data['Reservations']:
+            for instance in reservation['Instances']:
+                instances[instance['InstanceId']] = copy.deepcopy(instance)
+        return instances
+
+
+class TestExtractLogicClasses(unittest.TestCase):
+
+    def setUp(self):
+        self.client = MockBoto3Ec2Client()
+        self.root_raw_data = self.client.describe_instances()
+        self.instance_1 = self.root_raw_data['Reservations'][0]['Instances'][0]
+
+    def test_instance_extract_logic(self):
+        c = InstanceExtractLogic()
+        data = c.extract(self.root_raw_data)
+        self.assertIsNotNone(data)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(len(data), 3)
+        self.assertTrue('i-00000000000000000' in data)
+        self.assertTrue('i-11111111111111111' in data)
+        self.assertTrue('i-22222222222222222' in data)
+
+    def test_instance_name_extract_logic(self):
+        c = InstanceNameExtractLogic()
+        data = c.extract(raw_data=self.instance_1)
+        self.assertIsNotNone(data)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(len(data), 1)
+        self.assertTrue('Name' in data)
+        self.assertIsInstance(data['Name'], str)
+        self.assertFalse(data['Name'] == '-')
+        self.assertTrue(len(data['Name']) > 1)
+    
+    def test_instance_state_extract_logic(self):
+        c = InstanceStateExtractLogic()
+        data = c.extract(raw_data=self.instance_1)
+        self.assertIsNotNone(data)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data['Code'], 80)
+        self.assertEqual(data['Name'], 'stopped')
+
+
 class TestAwsEc2Scenario(unittest.TestCase):
     pass
+
 
 if __name__ == '__main__':
     unittest.main()
